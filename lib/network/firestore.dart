6b98316dbef2f71fc3e6e2ff881d78a8f5295abf
OmarTarekMohamedHaridy <OmarTarekMohamedHaridy@gmail.com>
2024-09-06 00:58:56 +0300

    firebase done

--- lib/network/firestore.dart
+++ lib/network/firestore.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:movies_app/model/detail/Details.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class FireStoreUtils{
   static CollectionReference<Details> getCollection(){
